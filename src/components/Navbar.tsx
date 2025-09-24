356cc9ce8d02bcaa3f3920329f039306b8d869cb
ashishpundir1997 <ashishpundir1997@gmail.com>
2025-09-24 07:20:51 +0530

    fix: padding in mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import WalletConnectButton from "./common/WalletConnectButton";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full h-[64px] flex items-center justify-between px-6 z-50 bg-[#212124]">
+    <nav className="fixed top-0 left-0 w-full h-[64px] flex items-center justify-between px-4 lg:px-6 z-50 bg-[#212124]">
       {/* Left Icon */}
      <div className="flex items-center gap-3">
       <img src={TokenIcon} alt="Token Icon" className="cursor-pointer w-8 h-8 lg:w-10 lg:h-10" />
